473cf3ce0b5afa0d026dfa82bce19696a136afc8
Sundus <sundusaden105@gmail.com>
2023-02-28 11:19:40 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,6 @@ puts "🌱 Seeding data..."
       comment: Faker::Lorem.sentence,
       game_id: game.id # use the ID (primary key) of the game as the foreign key
     )
-  end
-end
-
+  
+    
 puts "🌱 Done seeding!"
